84a6854204e16740b98f7f970e838aa72645e37b
xenups <xenups@outlook.com>
2019-06-22 01:03:40 +0430

    #dockerized project -- fixed redis

--- villaStore/settings.py
+++ villaStore/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis:6379/",
+        # "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "CONNECTION_POOL_KWARGS": {"max_connections": 100, "retry_on_timeout": True},
